bb191f817d4778301456c36ff9a74c6222edc055
Lorenzo Tablante <lorenzotablante@LorenzosMac.local>
2018-04-27 18:50:51 +1000

    First Commit

---
+++ Assets/moveobject.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+
+public class moveobject : MonoBehaviour {
+
+	public float speed = 6f;
+	public float JumpForce = 6f;
+	public Rigidbody rb;
+
+
+	// Use this for initialization
+	void Start () {
+		rb = GetComponent<Rigidbody> ();
+	}
+
+	// Update is called once per frame
+	void Update () {
+		transform.Translate (Input.GetAxis ("Horizontal") * speed * Time.deltaTime, 0f, Input.GetAxis ("Vertical") * speed * Time.deltaTime);
+
+		if (Input.GetButtonDown("Jump")) {
+			rb.velocity = new Vector3 (0f, JumpForce, 0f);
+		}
+	}
+}
